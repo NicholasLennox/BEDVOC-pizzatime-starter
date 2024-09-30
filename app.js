80652e6b93fc6055ecd2f55f272423391f2f1ab2
Madmadz16 <mfthaulow@gmail.com>
2024-09-30 12:40:05 +0200

    feat(task2): added order summary

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const pizzaSize = document.getElementById('pizza-size');
 const amount = document.getElementById('amount');
 const addBtn = document.getElementById('btn-submit');
 const clearBtn = document.getElementById('btn-clear');
+const orderList = document.getElementById('order-list');
+const orderTotal= document.getElementById('order-total');
 const orderPizzas = [];
 
 addBtn.addEventListener('click', addOrder);
@@ -10,9 +12,9 @@ clearBtn.addEventListener('click', clearOrder);
 
 function addOrder(event) {
 	event.preventDefault();
-    const toppings = Array.from(
-        document.querySelectorAll('input[name="toppings"]:checked')
-    ).map((x) => x.value.split('-')[0]);
+	const toppings = Array.from(
+		document.querySelectorAll('input[name="toppings"]:checked')
+	).map((x) => x.value.split('-')[0]);
 	const type = pizzaType.value.split('-');
 	const size = pizzaSize.value.split('-');
 
@@ -29,13 +31,25 @@ function addOrder(event) {
 		toppingsPrice: toppings.length * 10,
 		quantity: amount.value,
 		pizzaTotalPrice: totalPrice,
-    };
-    orderPizzas.push(pizzaOrder)
-	console.log(pizzaOrder);
-	// console.log(type);
-	// console.log(size);
-	// console.log(toppings);
-	// console.log(amount.value);
+	};
+	orderPizzas.push(pizzaOrder);
+	updateOrderSummary();
+	this.form.reset();
 }
 
 function clearOrder(event) {}
+
+function updateOrderSummary() {
+	const orderItem = document.createElement('li');
+	let str = document.createTextNode(
+		`${orderPizzas[orderPizzas.length-1].quantity} x ${
+			orderPizzas[orderPizzas.length-1].pizzaName
+		} - ${orderPizzas[orderPizzas.length-1].pizzaTotalPrice} NOK`
+	);
+
+	orderItem.appendChild(str);
+    orderList.appendChild(orderItem);
+    let tot = parseInt(orderTotal.innerHTML);
+    tot += orderPizzas[orderPizzas.length-1].pizzaTotalPrice;
+    orderTotal.innerText = tot
+}
