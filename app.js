61bb5763857e5423cf833b7f20d6d61f2cd630e2
Madmadz16 <mfthaulow@gmail.com>
2024-09-30 14:34:03 +0200

    feat(task3): added credit card verification

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const amount = document.getElementById('amount');
 const addBtn = document.getElementById('btn-submit');
 const clearBtn = document.getElementById('btn-clear');
 const orderList = document.getElementById('order-list');
-const orderTotal= document.getElementById('order-total');
-const orderPizzas = [];
+const orderTotal = document.getElementById('order-total');
+const payBtn = document.getElementById('submit-payment');
+const cardNumber = document.getElementById('credit-card');
+const paymentMsg = document.getElementById('payment-message');
+let orderPizzas = [];
 
 addBtn.addEventListener('click', addOrder);
 clearBtn.addEventListener('click', clearOrder);
+payBtn.addEventListener('click', pay);
 
 function addOrder(event) {
 	event.preventDefault();
@@ -37,19 +41,68 @@ function addOrder(event) {
 	this.form.reset();
 }
 
-function clearOrder(event) {}
+function clearOrder(event) { 
+    event.preventDefault();
+    orderPizzas = [];
+    orderList.innerHTML = ''
+    orderTotal.innerHTML = 0
+}
+
+function pay(event) {
+    event.preventDefault();
+    const cardNum = cardNumber.value.replace(/[^0-9]/g, '');
+    const valid = checkLuhn(cardNum)
+    if (!valid) return
+    const obj = {
+		pizzas: orderPizzas,
+        total: orderTotal.innerHTML,
+        cardNumber: cardNum
+    };
+    console.log(obj)
+}
+
+function checkLuhn(num) {
+	let last = num.slice(-1);
+	num = num.slice(0, -1).split('').reverse().join('');
+	const arr = [];
+	for (let i = 0; i < num.length; i++) {
+		if (i % 2 == 0) {
+			let temp = parseInt(num[i]) * 2;
+			if (temp > 9)
+				arr.push(
+					temp
+						.toString()
+						.split('')
+						.reduce((sum, digit) => sum + parseInt(digit), 0)
+				);
+			else arr.push(temp);
+		} else arr.push(parseInt(num[i]));
+	}
+	let sum = arr.reduce((sum, digit) => sum + digit, 0);
+	let final = num + (10 - (sum % 10));
+
+	if (final === num + last) {
+		paymentMsg.innerText = 'Payment successful!';
+        paymentMsg.style.color = 'green';
+        return true
+	} else {
+		paymentMsg.innerText = 'Invalid credit card number.';
+        paymentMsg.style.color = 'red';
+        return false
+	}
+}
 
 function updateOrderSummary() {
 	const orderItem = document.createElement('li');
 	let str = document.createTextNode(
-		`${orderPizzas[orderPizzas.length-1].quantity} x ${
-			orderPizzas[orderPizzas.length-1].pizzaName
-		} - ${orderPizzas[orderPizzas.length-1].pizzaTotalPrice} NOK`
+		`${orderPizzas[orderPizzas.length - 1].quantity} x ${
+			orderPizzas[orderPizzas.length - 1].pizzaName
+		} - ${orderPizzas[orderPizzas.length - 1].pizzaTotalPrice} NOK`
 	);
 
 	orderItem.appendChild(str);
-    orderList.appendChild(orderItem);
-    let tot = parseInt(orderTotal.innerHTML);
-    tot += orderPizzas[orderPizzas.length-1].pizzaTotalPrice;
-    orderTotal.innerText = tot
+	orderList.appendChild(orderItem);
+	let tot = parseInt(orderTotal.innerHTML);
+	tot += orderPizzas[orderPizzas.length - 1].pizzaTotalPrice;
+	orderTotal.innerText = tot;
 }
